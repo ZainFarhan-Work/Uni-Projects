53145cf960b8ebabd8a694344d79d0bae6672abe
ZainFarhan-Work <zainfarhan.work@gmail.com>
2025-02-13 15:52:34 +0500

    Nothing works :(

--- PA2/src/locator.c
+++ PA2/src/locator.c
@@ -12,10 +12,23 @@ typedef struct Locator {
 
 Locator* initializeMap(int num_allocations, void* addr, char* identifier)
 {
-    Locator* map = (Locator*) malloc(num_allocations * sizeof(Locator));
+    Locator* map = (Locator*) malloc((num_allocations + 2) * sizeof(Locator));
 
-    map->identifier = identifier;
-    map->address = (uintptr_t) addr;
+    if (map == NULL)
+    {
+        return NULL;
+    }
+    
+    for (int i = 0; i < num_allocations + 1; i++)
+    {
+        map[i].identifier = NULL;
+        map[i].address = NULL;
+    }
+
+    map[0].identifier = identifier;
+    map[0].address = (uintptr_t) addr;
+
+    map[num_allocations + 1].identifier = "END";
 
     return map;
     
@@ -27,17 +40,67 @@ void releaseMap(Locator* map){
 
 void clearMap(Locator* map)
 {
-    // free(map);
+    free(map);
     
 }
 
-Locator* resizeMap(Locator* map, int new_num_allocs){
+Locator* resizeMap(Locator* map, int new_num_allocs)
+{
+    Locator* new_map = initializeMap(new_num_allocs, NULL, NULL);
+
+    Locator* temp;
+    int count = -1;
+    printf("Here");
+    
+    do
+    {
+        count++;
+        *temp = map[count];
+
+        if (temp->identifier != "END")
+        {
+            new_map[count] = *temp;
+        }
+        
+
+    } while (temp->identifier != "END");
+
+    clearMap(map);
+
+    return new_map;
     
 }
 
 void makeEntry(Locator** map, void* addr, char* identifier)
 {
+    int count = -1;
+    Locator* temp;
+
+    do
+    {
+        count++;
+        temp = *map + count;
+
+    } while (/*temp->identifier != NULL &&*/ temp->identifier != "END");
+
+    if (temp->identifier == "END")
+    {
+        printf("Here: %d", count);
+        *map = resizeMap(*map, (count - 1) * 2);
+
+        count = -1;
+
+        do
+        {
+            count++;
+            temp = *map + count;
+
+        } while (temp->identifier != NULL && temp->identifier != "END");
+
+    }
     
+    temp->identifier = identifier;
+    temp->address = (uintptr_t) addr;
     
 }
 
