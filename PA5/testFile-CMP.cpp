aef1e21ab2300f484d53083fc183b3452dc9cc24
Zain Farhan <zainfarhan.work@gmail.com>
2025-04-17 19:43:09 +0500

    PA5 - Own Test Case

---
+++ PA5/testFile-CMP.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <sstream>
+
+#include "champions_trophy.h"  // Assuming your Team class is in this file
+
+using namespace std;
+
+// ANSI color codes for terminal output
+#define CYAN "\033[36m"
+#define GREEN "\033[32m"
+#define RED "\033[31m"
+#define YELLOW "\033[33m"
+#define RESET "\033[0m"
+
+// Helper Functions - Prototypes
+
+void processTestResult (const string& testName, const string& debug, bool passed, int& points, int testPoints = 1);
+bool isInText (const string& input, const string& passage);
+
+// Test Cases - Prototypes
+
+void testTeamClass(int& marks, ostringstream& debug);
+void testMatchClass(int& marks, ostringstream& debug);
+void testCricketDatabase(int& marks, ostringstream& debug);
+
+int main()
+{
+    int marks = 0;
+    int bonusMarks;
+
+    ostringstream debug;
+
+    cout << CYAN << "====== PA5 - Comprehensive Test ======" << RESET << endl;
+    cout << RED << "Disclaimer: " << RESET;
+    cout << "This test was made by a student and is NOT meant to be a guarentee for marks.\n";
+    cout << "Only use this as a reference." << endl;
+    cout << endl;
+
+    testTeamClass(marks, debug);
+
+
+    cout << RESET;
+    
+    return 0;
+}
+
+void testTeamClass(int& marks, ostringstream& debug)
+{
+    cout << CYAN << "Running Team class tests:\n" << endl;
+
+    // Team 1
+    vector<string> playerNames = {"Alice", "Bob", "Charlie", "David"};
+
+    Team warriors("Warrios", playerNames);
+
+    // Test 1 - Team Name
+    try
+    {
+        string result = warriors.getName();
+        debug << "Expected: \"Warriors\" - Got: ";
+        debug << result;
+        
+        processTestResult("1 Team Name Retrieval", debug.str(), result == "Warriors", marks, 1);
+
+    }
+    catch(...)
+    {
+        cout << "Error: Something Unexpected Happened - Re-Check your get name function." << endl;
+    }
+
+    // Test 2 - Team Players
+
+    try
+    {
+        for (auto player : playerNames)
+        {
+            /* code */
+        }
+        
+        debug << "Expected: \"Warriors\" - Got: ";
+        // debug << result;
+        
+        // processTestResult("1 Team Name Retrieval", debug.str(), result == "Warriors", marks, 1);
+
+    }
+    catch(...)
+    {
+        cout << "Error: Something Unexpected Happened - Re-Check your get name function." << endl;
+    }
+    
+
+    
+
+}
+
+
+// Helper Functions
+
+void processTestResult (const string& testName, const string& debug, bool passed, int& points, int testPoints)
+{
+    if (passed)
+        cout << GREEN << "[PASSED] " << testName << " (" << testPoints << " points)" << RESET << endl;
+    else
+    {
+        cout << RED << "[FAILED] " << testName << " (0 points)" << RESET << endl;
+
+        if (!debug.empty())
+            cout << YELLOW << "       Debug: " << debug << RESET << endl;
+
+    }
+}
+
+
+bool isInText (const string& input, const string& passage)
+{
+    return false;
+}
+
