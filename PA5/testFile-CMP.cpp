fc6d2e9bce06a18c0aee27f6f7e2c890a8ff1b8a
Zain Farhan <zainfarhan.work@gmail.com>
2025-04-18 23:20:42 +0500

    PA5 - Own Test Case

--- PA5/testFile-CMP.cpp
+++ PA5/testFile-CMP.cpp
@@ -51,7 +51,7 @@ void testTeamClass(int& marks, ostringstream& debug)
     // Team 1
     vector<string> playerNames = {"Alice", "Bob", "Charlie", "David"};
 
-    Team warriors("Warrios", playerNames);
+    Team warriors("Warriors", playerNames);
 
     // Test 1 - Team Name
     try
@@ -68,28 +68,120 @@ void testTeamClass(int& marks, ostringstream& debug)
         cout << "Error: Something Unexpected Happened - Re-Check your get name function." << endl;
     }
 
+    debug.str("");
+
     // Test 2 - Team Players
 
     try
     {
-        for (auto player : playerNames)
+        
+        debug << "Expected: True - Got: " << warriors.hasPlayer("Alice");
+        processTestResult("2.1 Player Exists - Alice", debug.str(), warriors.hasPlayer("Alice"), marks, 1);
+        debug.str("");
+
+        debug << "Expected: True - Got: " << warriors.hasPlayer("Alice");
+        processTestResult("2.2 Player Exists - Charlie", debug.str(), warriors.hasPlayer("Charlie"), marks, 1);
+        debug.str("");
+
+        debug << "Expected: True - Got: " << warriors.hasPlayer("Alice");
+        processTestResult("2.3 Player Does Not Exist - Eve", debug.str(), !warriors.hasPlayer("Eve"), marks, 1);
+
+    }
+    catch(...)
+    {
+        cout << "Error: Something Unexpected Happened - Re-Check your Get Player function." << endl;
+    }
+
+    debug.str("");
+
+    try
+    {
+        bool pass = true;
+        string expected = "";
+        string got = "";
+
+        for (int i = 0; i < playerNames.size(); i++)
         {
-            /* code */
+            if (playerNames[i] != warriors.getPlayers()[i])
+            {
+                bool pass = false;
+            }
+
+            expected += playerNames[i] + " ";
+            got += warriors.getPlayers()[i] + " ";
+            
         }
+
+        debug << "Expected: ";
+        debug << expected;
+        debug << "- Got: ";
+        debug << got;
         
-        debug << "Expected: \"Warriors\" - Got: ";
-        // debug << result;
+        processTestResult("2.4 Team Player Names Retrieval", debug.str(), pass, marks, 2);
+
+    }
+    catch(...)
+    {
+        cout << "Error: Something Unexpected Happened - Re-Check your Get Player function." << endl;
+    }
+
+    debug.str("");
+
+    // Test 3 - Wins and Match Count
+
+    try
+    {
+        int result1 = warriors.getWins();
+        int result2 = warriors.getTotalMatches();
+        debug << "Expected: Wins = 0 and Total Matches = 0 - Got: ";
+        debug << "Wins = ";
+        debug << warriors.getWins();
+        debug << " and Total Matches = ";
+        debug << warriors.getTotalMatches();
         
-        // processTestResult("1 Team Name Retrieval", debug.str(), result == "Warriors", marks, 1);
+        processTestResult("3.1 Initial Wins and Matches", debug.str(), result1 == 0 && result2 == 0, marks, 1);
+
+        debug.str("");
+
+        warriors.addMatch();
+        warriors.addMatch();
+        warriors.addWin();  // 1 win out of 2 matches
+
+        result1 = warriors.getWins();
+        result2 = warriors.getTotalMatches();
+        debug << "Expected: Wins = 0 and Total Matches = 0 - Got: ";
+        debug << "Wins = ";
+        debug << warriors.getWins();
+        debug << " and Total Matches = ";
+        debug << warriors.getTotalMatches();
+        
+        processTestResult("3.2 Updated Wins and Matches", debug.str(), result1 == 1 && result2 == 2, marks, 1);
+
+        debug.str("");
+
+        debug << "Expected: Wins Percentage = 50 % - Got: ";
+        debug << warriors.getWinPercentage() << " %";
+
+        processTestResult("3.3 Win Percentage", debug.str(), warriors.getWinPercentage() == 50, marks, 1);
+
+        warriors.addMatch();
+        warriors.addWin();
+
+        debug.str("");
+
+        debug << "Expected: Wins Percentage = 66 % - Got: ";
+        debug << warriors.getWinPercentage() << " %";
+
+        processTestResult("3.3 Updated Wins and Matches", debug.str(), warriors.getWinPercentage() == 66, marks, 1);
 
     }
     catch(...)
     {
         cout << "Error: Something Unexpected Happened - Re-Check your get name function." << endl;
     }
-    
 
-    
+    cout << "\nTeam Class tests completed. Take a short break :)\n";
+    cout << CYAN << "Points: " << marks << " / 10" << RESET << endl;
 
 }
 
@@ -99,7 +191,10 @@ void testTeamClass(int& marks, ostringstream& debug)
 void processTestResult (const string& testName, const string& debug, bool passed, int& points, int testPoints)
 {
     if (passed)
+    {
         cout << GREEN << "[PASSED] " << testName << " (" << testPoints << " points)" << RESET << endl;
+        points += testPoints;
+    }
     else
     {
         cout << RED << "[FAILED] " << testName << " (0 points)" << RESET << endl;
