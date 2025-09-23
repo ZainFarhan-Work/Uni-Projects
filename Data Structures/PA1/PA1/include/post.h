464c13ff50f29713f8e20ecffc245b69862f1c8b
zain <zainfarhan.work@gmail.com>
2025-09-23 10:22:23 +0500

    DS - PA1: Post Pool list

--- Data Structures/PA1/PA1/include/post.h
+++ Data Structures/PA1/PA1/include/post.h
@@ -6,12 +6,12 @@ using UserID = int;
 using PostID = int;
 
 struct Post {
-    PostID postID;
-    string category;
-    int views;
-    string content;
+  PostID postID;
+  string category;
+  int views;
+  string content;
 
-    // metadata for linking inside lists (optional if wrapper node used)
-    Post(PostID id = 0, const string& cat = "", int v = 0, const string& content="")
-      : postID(id), category(cat), views(v), content(content) {}
+  // metadata for linking inside lists (optional if wrapper node used)
+  Post(PostID id = 0, const string& cat = "", int v = 0, const string& content="")
+    : postID(id), category(cat), views(v), content(content) {}
 };
--- Data Structures/PA1/PA1/src/post_list.cpp
+++ Data Structures/PA1/PA1/src/post_list.cpp
@@ -1,26 +1,123 @@
 #include "../include/post_list.h"
 #include <iostream>
 
-PostList::~PostList() {
+PostList::~PostList()
+{
+
+    PostNode* prev = nullptr;
+    PostNode* node = head;
+
+    while (node)
+    {
+        prev = node;
+        node = node->next;
+
+        delete prev;
+    }
+
+    head = nullptr;
+
+}
+
+void PostList::addPost(const Post& p)
+{
+    Post* temp = new Post(p.postID, p.category, p.views, p.content);
+    PostNode* node = new PostNode(temp);
+
+    if (head)
+    {
+        node->next = head;
+        head = node;
+    }
+    else
+    {
+        head = node;
+    }
+}
+
+bool PostList::removePost(int postID)
+{
+    if (isEmpty())
+    {
+        return false;
+    }
+
+    PostNode* prev = nullptr;
+    PostNode* node = head;
+
+    while (node && node->post->postID != postID)
+    {
+        prev = node;
+        node = node->next;
+    }
+
+    if (!node)
+    {
+        return false;
+    }
+
+    if (prev)
+    {
+        prev->next = node->next;
+    }
+    else
+    {
+        head = node->next;
+    }
+    
+
+    delete node;
+    node = nullptr; // I don't think this is strictly neccessary but just in case
+
+    return true;
     
 }
 
-void PostList::addPost(const Post& p) {
+Post* PostList::findPost(int postID)
+{
+    if (isEmpty())
+    {
+        return nullptr;
+    }
+
+    PostNode* node = head;
+
+    while (node && node->post->postID != postID)
+    {
+        node = node->next;
+
+        if (node && node->post->postID == postID)
+        {
+            return node->post;
+        }
+        
+    }
+
+    return nullptr;
     
 }
 
-bool PostList::removePost(int postID) {
+void PostList::displayPosts() const
+{
+    if (isEmpty())
+    {
+        cout << "<EMPTY>" << endl;
+    }
+
+    PostNode* node = head;
+
+    while (node)
+    {
+        cout << "ID: " << node->post->postID << endl;
+        cout << "Category: " << node->post->category << endl;
+        cout << "Views: " << node->post->views << endl;
+        cout << "Content: " << node->post->content << endl;
+
+        node = node->next;
+        
+    }
+    
     
 }
 
-Post* PostList::findPost(int postID) {
-    
-}
-
-void PostList::displayPosts() const {
-    
-}
-
-bool PostList::isEmpty() const {
-    
-}
+bool PostList::isEmpty() const { return head == nullptr; }
